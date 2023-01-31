6b7b398480ca6572f87a1960503eceea1f2bbabb
peytia <parchiba@calpoly.edu>
2023-01-31 13:06:07 -0800

    Create motor_controller.py

---
+++ src/motor_controller.py
@@ -0,0 +1,38 @@
+"""!
+    @file                       motor_controller.py
+    @brief                      This class implements
+    @details                    This is a driver for 
+                                
+    @author                     Peyton Archibald
+    @author                     Harrison Hirsch
+    @date                       January 31, 2023
+"""
+
+    """!
+    @brief                      This class implements 
+    @details                    This is a driver for 
+    """
+
+    def __init__(self, en_pin, in1pin, in2pin, timer):
+        """!
+            @brief              Constructs an controller object
+            @details            Upon instantiation, the controller object 
+            @param  en_pin      The
+            @param  in1pin      The
+            @param  in2pin      The  
+        """  
+
+        
+        print("Creating a motor controller")
+        
+    def run(self):
+        pass
+    
+    def set_setpoint(self):
+        pass
+    
+    def set_Kp(self):
+        pass
+    
+    
+        
