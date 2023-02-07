f2a86c0666172e262ef525146b6c41e48df41597
harrisonhirsch <harrisonhirsch@gmail.com>
2023-02-07 13:37:06 -0800

    Add type and index checking

--- temp/CPython_serial_readandplot.py
+++ temp/CPython_serial_readandplot.py
@@ -9,16 +9,18 @@ def main():
         plt.close()
         data_list_x = []
         data_list_y = []
-        # line = 0
-        while True:
-            if ser.inWaiting() > 0:
-                data = ser.readline().strip(b'\r\n').split(b', ')
-                data_list_x.append(float(data[0]))
-                data_list_y.append(float(data[1]))
-                if len(data_list_x) >= 600:
-                    break
-            else:
-                time.sleep(0.01)
+        try:
+            while True:
+                if ser.inWaiting() > 0:
+                    data = ser.readline().strip(b'\r\n').split(b', ')
+                    data_list_x.append(float(data[0]))
+                    data_list_y.append(float(data[1]))
+                    if len(data_list_x) >= 600:
+                        break
+                else:
+                    time.sleep(0.01)
+        except (ValueError, IndexError) as e:
+            print(e)
     print('checkpoint')
     plt.plot(data_list_x, data_list_y, 'r-')
     plt.xlabel('Time [ms]')
@@ -27,12 +29,12 @@ def main():
     plt.show()
 
 
-def is_number(pt):          # Helper function to test if number
-    try:
-        float(pt)
-        return True
-    except ValueError:
-        return False
+# def is_number(pt):          # Helper function to test if number
+#     try:
+#         float(pt)
+#         return True
+#     except ValueError:
+#         return False
 
 
 if __name__ == '__main__':
