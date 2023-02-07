2a436cd6080a8fcb4bf512412d6611c4cf466972
harrisonhirsch <harrisonhirsch@gmail.com>
2023-02-07 13:41:24 -0800

    Position ss value

--- temp/CPython_serial_readandplot.py
+++ temp/CPython_serial_readandplot.py
@@ -25,7 +25,7 @@ def main():
     plt.plot(data_list_x, data_list_y, 'r-')
     plt.xlabel('Time [ms]')
     plt.ylabel('Position [encoder counts]')
-    plt.annotate(f'Steady State: {data_list_y[len(data_list_y)-1]} Counts', xy=(data_list_x[len(data_list_x)-1], data_list_y[len(data_list_y)-1]))
+    plt.annotate(f'{data_list_y[len(data_list_y)-1]} Counts', xy=(data_list_x[len(data_list_x)-1]-1000, data_list_y[len(data_list_y)-1]-5000))
     plt.show()
 
 
